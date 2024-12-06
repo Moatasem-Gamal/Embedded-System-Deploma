ed2aa87827951ff74dd4a4d57f49fdfd337eecb7
moatasem <mtsmgmal@gmail.com>
2024-12-06 11:37:09 +0200

    first task

---
+++ Homework_1_solution/EX1/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 3, 2024
+ *      Author: moatasem
+ */
+
+#include "stdio.h"
+
+void main ()
+{
+	printf ("C Programming");
+
+}
---
+++ Homework_1_solution/EX3/mian.c
@@ -0,0 +1,19 @@
+/*
+ * mian.c
+ *
+ *  Created on: Dec 3, 2024
+ *      Author: moatasem
+ */
+
+#include "stdio.h"
+
+void main()
+{
+	int x , y , sum ;
+	printf ("Enter first number : ") ;
+	scanf ("%i" , x ) ;
+	printf ("Enter second number : ") ;
+	scanf ("%i" , y ) ;
+	sum = x + y ;
+	printf ("sum = %i" , sum);
+}
